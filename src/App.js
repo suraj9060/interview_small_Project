c7393e62735e7e83889770db737a043092367ab0
suraj9060 <87435542+suraj9060@users.noreply.github.com>
2022-05-22 22:09:36 +0530

    stop watch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { useState , useEffect} from "react"
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [status, setStatus] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (status) {
+      interval = setInterval(() => {
+        setTime((PrivTime) => PrivTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [status]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}</span>:
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}</span>:
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </h2>
+      <button onClick={() => setStatus(true)}>start</button>
+      <button onClick={() => setStatus(false)}>stop</button>
+      <button onClick={() => setStatus(true)}>resume</button>
+      <button onClick={() => setTime(0)}>reset</button>
     </div>
   );
 }
+
 
 export default App;
